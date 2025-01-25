e2b532f10b282184e0b362722a5ec2909ccc794d
2023aa05455@wilp.bits-pilani.ac.in <yogesh_chavhan@bmc.com>
2025-01-25 14:49:39 +0530

    added tests for testing stage

---
+++ tests/test_cancer_detection_model.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from mlops.src.model.cancer_detaction_model import train_and_evaluate, preprocess_data,load_data,CONFIG
+
+class Train_and_Evaluate_Model(TestCase):
+    def test_train_and_evaluate(self):
+        # Load the data
+        X, y = load_data()
+        # Preprocess data
+        X_train, X_test, y_train, y_test = preprocess_data(X, y)
+        # Train Model
+        for model_name, model_config in CONFIG["models"].items():
+            model, accuracy, precision, f1 = train_and_evaluate(model_name, model_config, X_train, X_test, y_train, y_test)
+            assert accuracy > 0.97, f"Model accuracy should be >0.9, got {accuracy}"
