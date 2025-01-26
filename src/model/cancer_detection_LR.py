f6f2fa84e811a8030a25c1a4be5193c47481905d
2023aa05455@wilp.bits-pilani.ac.in <yogesh_chavhan@bmc.com>
2025-01-26 10:13:16 +0530

    added models for LR and dockerfile

---
+++ src/model/cancer_detection_LR.py
@@ -0,0 +1,83 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, f1_score
+
+# Configuration
+CONFIG = {
+    "random_seed": 42,
+    "test_size": 0.2,
+    "models": {"LogisticRegression": {"solver": "liblinear"},
+               },
+}
+
+
+# Step 1: Load dataset
+
+
+def load_data():
+    data = load_breast_cancer()
+    df = pd.DataFrame(data.data, columns=data.feature_names)  # Check the shape
+    # (number_of_samples, number_of_features)
+    print("Dataset shape:", df.shape)
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = pd.Series(data.target, name="target")
+    print("x",X)
+    print("y",y)
+    return X, y
+
+
+# Step 2: Preprocess the data
+
+
+def preprocess_data(X, y):
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_scaled, y, test_size=0.2, random_state=42)
+
+    return X_train, X_test, y_train, y_test
+
+
+# Step 3: Train and evaluate models
+
+
+def train_and_evaluate(X_train, X_test, y_train, y_test):
+    model = LogisticRegression(solver="liblinear", random_state=42)
+
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print("Prediction:",y_pred)
+    # Calculate metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    return model, accuracy, precision, f1
+
+
+def main():
+    # Load and preprocess data
+    X, y = load_data()
+    X_train, X_test, y_train, y_test = preprocess_data(X, y)
+    print(f"Training and evaluating LogisticRegression Model ..!")
+
+    # Train and evaluate
+    model, accuracy, precision, f1 = train_and_evaluate(X_train, X_test, y_train, y_test)
+    # Log parameters and metrics
+    print(
+        f"------LogisticRegression Metrics ----- "
+        f"Accuracy: {accuracy:.2f}, "
+        f"Precision: {precision:.2f}, "
+        f"F1-score: {f1:.2f}"
+    )
+
+
+if __name__ == "__main__":
+    main()
